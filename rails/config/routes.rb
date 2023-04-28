105e1c1e696a8d3db6065c09c13f252f5e2e6f14
Yuki Ishikawa <hoto@y32.jp>
2023-04-28 16:01:24 +0900

    static pages

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
   resources :microposts
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
